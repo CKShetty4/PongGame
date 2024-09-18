5240492dac6af1e0932bbedc94ea31b88e5d395b
CKShetty4 <ckshetty004@gmail.com>
2024-09-18 10:40:17 +0530

    Movement added for  the ball

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include "raylib.h"
 #include <iostream>
 using namespace std;
+
+class Ball{
+    public:
+    float x,y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Move(){
+        x+=speed_x;
+        y+=speed_y;
+    }
+};
+
+Ball ball;
+
 int main() {
     cout<<"Starting the game"<<endl;
     const int screenWidth = 1280;
@@ -8,11 +27,22 @@ int main() {
     InitWindow(screenWidth, screenHeight, "Pong Game");
     SetTargetFPS(60);
 
+ball.radius=20;
+ball.x=screenWidth/2;
+ball.y=screenHeight/2;
+ball.speed_x=7;
+ball.speed_y=7;
+
+
     while (WindowShouldClose()==false)
     {
         BeginDrawing();
+//upating 
+        ball.Move();
+//Drawing
+ClearBackground(BLACK);//To fill the window with black color before drawing anything.. So that the previous frame is not visible
         DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
-        DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
+        ball.Draw();
         DrawRectangle(10, screenHeight/2-60, 25, 120, WHITE);//left paddle 10=10(padding) 60=rectangle height
         DrawRectangle(screenWidth-35, screenHeight/2-60, 25, 120, WHITE);//right paddle 35=10(padding)+25(rectangle width)
         EndDrawing();
