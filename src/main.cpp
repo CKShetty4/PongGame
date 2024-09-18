3991c1f01192b3d45ffebc74b3163a4c71415b79
CKShetty4 <ckshetty004@gmail.com>
2024-09-18 10:45:55 +0530

    collision cheking for all boundary/edges

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,16 @@ class Ball{
     void Move(){
         x+=speed_x;
         y+=speed_y;
+
+        if (y+ radius >=GetScreenHeight()||y-radius<=0)
+        {
+            speed_y*=-1;
+        }
+        if (x+ radius >=GetScreenWidth()||x-radius<=0)
+        {
+            speed_x*=-1;
+        }
+
     }
 };
 
