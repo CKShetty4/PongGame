145217c24b68f2bd1d31995a9a07f7c52ea2cf48
CKShetty4 <ckshetty004@gmail.com>
2024-09-18 10:31:04 +0530

    Draw Paddles and ball

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,10 @@ int main() {
     while (WindowShouldClose()==false)
     {
         BeginDrawing();
-
+        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
+        DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
+        DrawRectangle(10, screenHeight/2-60, 25, 120, WHITE);//left paddle 10=10(padding) 60=rectangle height
+        DrawRectangle(screenWidth-35, screenHeight/2-60, 25, 120, WHITE);//right paddle 35=10(padding)+25(rectangle width)
         EndDrawing();
     }
     
